a1734e729f33df800887557b353cb5716a0fa411
Rendelle Bertes <118584640+zekiro-ox@users.noreply.github.com>
2024-11-08 20:19:21 +0800

    fixed

--- api/WeatherAPI.js
+++ api/WeatherAPI.js
@@ -8,7 +8,7 @@ const BASE_URL = "http://api.weatherapi.com/v1"; // Base URL for WeatherAPI
 export const getWeatherData = async (city) => {
   try {
     const response = await axios.get(
-      `${BASE_URL}/current.json?key=${API_KEY}&q=${city}`
+      `http://api.weatherapi.com/v1/current.json?key=1b205e9e853f47b2baa120744242409&q=${city}`
     );
     return response.data; // Return the weather data
   } catch (error) {
